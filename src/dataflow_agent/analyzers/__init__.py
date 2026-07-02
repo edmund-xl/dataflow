06b8d639eb2b186b6814f46b5a66325f1503ea31
edmund-xl <xl823.cn1@gmail.com>
2026-07-02 11:38:43 +0800

    Add security risk analyzer

--- src/dataflow_agent/analyzers/__init__.py
+++ src/dataflow_agent/analyzers/__init__.py
@@ -2,10 +2,28 @@ from __future__ import annotations
 
 from .indexes import AnalysisIndexes, build_analysis_indexes
 from .models import AnalysisFinding, AnalysisReportSummary
+from .security import (
+    analyze_security_risks,
+    permission_blast_radius,
+    security_control_gap_rows,
+    sensitive_data_flow_rows,
+    write_permission_blast_radius_json,
+    write_security_control_gap_report_csv,
+    write_security_risk_report,
+    write_sensitive_data_flow_report_md,
+)
 
 __all__ = [
     "AnalysisFinding",
     "AnalysisIndexes",
     "AnalysisReportSummary",
+    "analyze_security_risks",
     "build_analysis_indexes",
+    "permission_blast_radius",
+    "security_control_gap_rows",
+    "sensitive_data_flow_rows",
+    "write_permission_blast_radius_json",
+    "write_security_control_gap_report_csv",
+    "write_security_risk_report",
+    "write_sensitive_data_flow_report_md",
 ]
